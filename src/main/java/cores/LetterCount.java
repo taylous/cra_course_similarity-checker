eae197af9db037781f86ecf251bbf84c219b2203
taylous <taylous99@gmail.com>
2024-04-29 14:24:09 +0900

    [Add] 생성자 테스트
    기본 생성자가 동작 테스트 및
    비교할 문자열을 parameter로 넘겨서 멤버 변수에 할당 후
    getter로 가져오는 로직 및 테스트 함수를 구현하였습니다.

--- src/main/java/cores/LetterCount.java
+++ src/main/java/cores/LetterCount.java
@@ -1,4 +1,26 @@
 package cores;
 
+import java.lang.reflect.Constructor;
+
 public class LetterCount {
+
+    private String compareToStr1;
+    private String compareToStr2;
+
+    public LetterCount(String compareToStr1, String compareToStr2) {
+        this.compareToStr1 = compareToStr1;
+        this.compareToStr2 = compareToStr2;
+    }
+
+    public LetterCount() {
+        this("", "");
+    }
+
+    public String getStringToCompare1() {
+        return this.compareToStr1;
+    }
+
+    public String getStringToCompare2() {
+        return this.compareToStr2;
+    }
 }
--- src/test/java/LetterCountTest.java
+++ src/test/java/LetterCountTest.java
@@ -6,8 +6,19 @@ import static org.junit.jupiter.api.Assertions.*;
 class LetterCountTest {
 
     @Test
-    void create_letter_count_constructor() {
+    void 기본_생성자_테스트() {
         LetterCount letterCount = new LetterCount();
         assertNotNull(letterCount);
+    }
+
+    @Test
+    void 비교할_문자열_2개_입력_테스트() {
+        LetterCount letterCount = new LetterCount("abc", "def");
+
+        String expected1 = "abc";
+        String expected2 = "def";
+
+        assertEquals(expected1, letterCount.getStringToCompare1());
+        assertEquals(expected2, letterCount.getStringToCompare2());
     }
 }
