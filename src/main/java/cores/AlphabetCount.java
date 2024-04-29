8f92998a47a3077adf45ad6fbf216c141fb77c29
taylous <taylous99@gmail.com>
2024-04-29 16:23:48 +0900

    [Refactoring] character set을 만들어주는 함수 구현

--- src/main/java/cores/AlphabetCount.java
+++ src/main/java/cores/AlphabetCount.java
@@ -7,9 +7,13 @@ public class AlphabetCount {
 
     public AlphabetCount() {}
 
+    private Set<Character> createCharacterSet(String targetStr) {
+        return targetStr.chars().mapToObj(ch -> (char) ch).collect(Collectors.toSet());
+    }
+
     public double checkUsedAlphabet(String compareToStr1, String compareToStr2) {
-        Set<Character> str1Set = compareToStr1.chars().mapToObj(ch -> (char) ch).collect(Collectors.toSet());
-        Set<Character> str2Set = compareToStr2.chars().mapToObj(ch -> (char) ch).collect(Collectors.toSet());
+        Set<Character> str1Set = createCharacterSet(compareToStr1);
+        Set<Character> str2Set = createCharacterSet(compareToStr2);
 
         if(str1Set.containsAll(str2Set)) {
             return 40.0;
